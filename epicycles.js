5946aa4c72422dfd21c911ae9a2bcf5a3a635ce3
cabutchei <luropa_paz@hotmail.com>
2025-07-06 22:26:10 -0300

    fully working prototype

--- epicycles.js
+++ epicycles.js
@@ -9,28 +9,30 @@ class Epicycles {
         zx = xDft[i];
         zy = yDft[i];
         if (i == 0){
-            this.xCircles.push({center: {x: width/2, y: 0}, radius: zx.abs(), freq: i/xDft.length, theta: zx.arg()});
-            this.yCircles.push({center: {x: 0, y: height/2}, radius: zy.abs(), freq: i/xDft.length, theta: zy.arg()}); 
+            this.xCircles.push({center: {x: 0, y: 3*height/8}, radius: zx.abs(), freq: i*1/xDft.length, theta: zx.arg()});
+            this.yCircles.push({center: {x: -3*width/8, y: 0}, radius: zy.abs(), freq: i*1/xDft.length, theta: zy.arg()}); 
         }
         else{
             let prevXCircle = this.xCircles[i-1];
             let prevYCircle = this.yCircles[i-1];
-            this.xCircles.push({center: {x: prevXCircle.center.x + cos(prevXCircle.theta), y: prevXCircle.center.y + sin(prevXCircle.theta)}, radius: zx.abs(), freq: i/xDft.length, theta: zx.arg()});
-            this.yCircles.push({center: {x: prevYCircle.center.x + cos(prevYCircle.theta), y: prevYCircle.center.y + sin(prevYCircle.theta)}, radius: zy.abs(), freq: i/xDft.length, theta: zy.arg()});
+            this.xCircles.push({center: {x: prevXCircle.center.x + prevXCircle.radius * cos(prevXCircle.theta), y: prevXCircle.center.y + prevXCircle.radius * sin(prevXCircle.theta)}, radius: zx.abs(), freq: i/xDft.length, theta: zx.arg()});
+            this.yCircles.push({center: {x: prevYCircle.center.x + prevYCircle.radius * cos(prevYCircle.theta), y: prevYCircle.center.y + prevYCircle.radius * sin(prevYCircle.theta)}, radius: zy.abs(), freq: i/xDft.length, theta: zy.arg()});
         }
       }
+      this.xCircles.sort((a,b) => b.radius-a.radius);
+      this.yCircles.sort((a,b) => b.radius-a.radius);
     }
 
     updateCircles(fps){
-        this.xCircles[0].theta += 2*PI*this.xCircles[0].freq/fps;
-        this.yCircles[0].theta += 2*PI*this.yCircles[0].freq/fps; 
+        this.xCircles[0].theta += 2*PI*this.xCircles[0].freq;
+        this.yCircles[0].theta += 2*PI*this.yCircles[0].freq; 
         for (let i = 1; i < this.xCircles.length; i++){
             let xCircle = this.xCircles[i];
             let yCircle = this.yCircles[i]; 
             let prevXCircle = this.xCircles[i-1];
             let prevYCircle = this.yCircles[i-1];
-            xCircle.theta += 2*PI*xCircle.freq/fps;
-            yCircle.theta += 2*PI*yCircle.freq/fps;
+            xCircle.theta += 2*PI*xCircle.freq;
+            yCircle.theta += 2*PI*yCircle.freq;
             xCircle.center.x = prevXCircle.center.x + prevXCircle.radius * cos(prevXCircle.theta);
             xCircle.center.y = prevXCircle.center.y + prevXCircle.radius * sin(prevXCircle.theta);
             yCircle.center.x = prevYCircle.center.x + prevYCircle.radius * cos(prevYCircle.theta);
@@ -45,19 +47,23 @@ class Epicycles {
             if (i < this.xCircles.length - 1){
                 let nextXCircle = this.xCircles[i+1];
                 let nextYCircle = this.yCircles[i+1];
-                circle(xCircle.center.x, xCircle.center.y, xCircle.radius);
-                line(xCircle.center.x, xCircle.center.y, nextXCircle.center.x, nextXCircle.center.y)
-                circle(yCircle.center.x, yCircle.center.y, yCircle.radius);
-                line(yCircle.center.x, yCircle.center.y, nextYCircle.center.x, nextYCircle.center.y)
+                circle(ireframeX(xCircle.center.x), ireframeY(xCircle.center.y), 2*xCircle.radius);
+                line(ireframeX(xCircle.center.x), ireframeY(xCircle.center.y), ireframeX(nextXCircle.center.x), ireframeY(nextXCircle.center.y))
+                circle(ireframeX(yCircle.center.x), ireframeY(yCircle.center.y), 2*yCircle.radius);
+                line(ireframeX(yCircle.center.x), ireframeY(yCircle.center.y), ireframeX(nextYCircle.center.x), ireframeY(nextYCircle.center.y))
             }
             else{
-                circle(xCircle.center.x, xCircle.center.y, xCircle.radius);
-                line(xCircle.center.x, xCircle.center.y, xCircle.center.x + xCircle.radius * cos(xCircle.theta), xCircle.center.y + xCircle.radius * sin(xCircle.theta))
-                circle(yCircle.center.x, yCircle.center.y, yCircle.radius);
-                line(yCircle.center.x, yCircle.center.y, yCircle.center.x + yCircle.radius * cos(yCircle.theta), yCircle.center.y + yCircle.radius * sin(yCircle.theta))
+                circle(ireframeX(xCircle.center.x), ireframeY(xCircle.center.y), 2*xCircle.radius);
+                line(ireframeX(xCircle.center.x), ireframeY(xCircle.center.y), ireframeX(xCircle.center.x + xCircle.radius * cos(xCircle.theta)), ireframeY(xCircle.center.y + xCircle.radius * sin(xCircle.theta)))
+                circle(ireframeX(yCircle.center.x), ireframeY(yCircle.center.y), 2*yCircle.radius);
+                line(ireframeX(yCircle.center.x), ireframeY(yCircle.center.y), ireframeX(yCircle.center.x + yCircle.radius * cos(yCircle.theta)), ireframeY(yCircle.center.y + yCircle.radius * sin(yCircle.theta)))
             }
         }
     }
+    drawLines(){
+        let lastXCircle = this.xCircles.slice(-1)[0];
+        let lastYCircle = this.yCircles.slice(-1)[0];
+        line(ireframeX(lastXCircle.center.x), ireframeY(lastXCircle.center.y), ireframeX(lastXCircle.center.x), ireframeY(lastYCircle.center.y))
+        line(ireframeX(lastYCircle.center.x), ireframeY(lastYCircle.center.y), ireframeX(lastXCircle.center.x), ireframeY(lastYCircle.center.y))
+    }
   }
-
-// export {Epicycles}
--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let xdft;
 let ydft;
 let circles = [];
 let theta;
-let fps = 60;
+let fps = 200;
 let epicycles;
 
 function setup() {
@@ -35,14 +35,15 @@ function draw() {
   background(200,200);
   switch (state){
     case USER_DRAWING:
-      let point = createVector(mouseX - width / 2, mouseY - height / 2);
+      // let point = createVector(mouseX - width / 2, mouseY - height / 2);
+      let point = createVector(mouseX, mouseY);
       drawing.push(math.Complex(point.x, point.y));
       // complexDrawing.push(math.Complex(point.x, point.y));
       stroke(255);
       noFill();
       beginShape();
       for (let v of drawing) {
-        vertex(v.re + width / 2, v.im + height / 2);
+        vertex(v.re, v.im);
       }
       endShape();
       break;
@@ -50,18 +51,64 @@ function draw() {
     case FOURIER_DRAWING:
       if (!dft) {
         dft = true;
-        xDrawing = drawing.map(i => i.re);
-        yDrawing = drawing.map(i => i.im);
+        drawing.forEach(i => {i.re = reframeX(i.re); i.im = reframeY(i.im)});
+        xDrawing = drawing.map(i => math.Complex(i.re, 0));
+        yDrawing = drawing.map(i => math.Complex(0, i.im));
         let xDft = math.fft(xDrawing).map(num => num.div(xDrawing.length));
         let yDft = math.fft(yDrawing).map(num => num.div(xDrawing.length));
         epicycles = new Epicycles(xDft, yDft, fps);
       }
       else {
+        if (fourierDrawing.length == drawing.length){
+          fourierDrawing = [];
+        }
+        let v = createVector(epicycles.xCircles.slice(-1)[0].center.x, epicycles.yCircles.slice(-1)[0].center.y)
+        fourierDrawing.push(v);
         epicycles.drawCircles();
+        epicycles.drawLines();
+        stroke(255);
+        noFill();
+        beginShape();
+        for (let v of fourierDrawing) {
+          vertex(ireframeX(v.x), ireframeY(v.y));
+        }
+        endShape();
         epicycles.updateCircles(fps);
       }
       break;
 
   }
 
+}
+
+function reframeX(x){
+  return x-width/2;
+}
+
+function ireframeX(x){
+  return x+width/2;
+}
+
+function reframeY(y){
+  return height/2 - y;
+}
+
+function ireframeY(y){
+  return height/2 - y;
+}
+
+function reframeAxes(x,y){
+  return {x: reframeX(x), y: reframeY(y)};
+}
+
+function ireframeAxes(x,y){
+  return {x: ireframeX(x), y: ireframeY(y)}
+}
+
+function invfft(circles){
+  let r = 0;
+  circles.forEach(circ => {
+    r = (math.exp((math.i).mul(circ.theta)).mul(circ.radius)).add(r)
+  })
+  return r
 }
